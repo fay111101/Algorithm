a76b7b2e5d36ca204588689bd4b3735f53764a7c
fay111101 <1139598278@qq.com>
2017-12-19 19:37:57 +0800

    20171219

---
+++ test/src/main/java/leetcode/dpandgreedy/JumpGame2.java
@@ -0,0 +1,27 @@
+package main.java.leetcode.dpandgreedy;
+
+/**
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+ Each element in the array represents your maximum jump length at that position.
+
+ Your goal is to reach the last index in the minimum number of jumps.
+
+ For example:
+ Given array A =[2,3,1,1,4]
+
+ The minimum number of jumps to reach the last index is2. (Jump1step from index 0 to 1, then3steps to the last index.)
+ */
+public class JumpGame2 {
+    public int jump(int[] A) {
+        int jumps = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < A.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + A[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+            }
+        }
+        return jumps;
+    }
+}
