c5329e93f8196850f2359b133abeba8a7546a8b5
fay111101 <1139598278@qq.com>
2018-11-15 23:33:43 +0800

    20181115

---
+++ src/main/java/leetcode/sort/AgeSort.java
@@ -0,0 +1,43 @@
+package leetcode.sort;
+
+import java.util.Arrays;
+
+public class AgeSort {
+    public static void main(String[] args) {
+
+        int[] ages = new int[] { 23, 45, 32, 43, 21, 24, 25, 23, 22, 22, 21 };
+        System.out.println("原数组为：" + Arrays.toString(ages));
+        AgeSort as = new AgeSort();
+        as.sortAge(ages);
+        System.out.println("排序后的数组为：" + Arrays.toString(ages));
+    }
+
+    private void sortAge(int[] ages) {
+
+        if (ages == null || ages.length < 1) {
+            return;
+        }
+        int oldAge = 80;
+        int youngAge = 20;
+
+        // 初始化一个odlAge+1的数组
+        int[] timeOfAge = new int[oldAge + 1];
+        // 将数组元素都置为0
+        for (int i = 0; i < timeOfAge.length; i++) {
+            timeOfAge[i] = 0;
+        }
+        // 某个年龄出现了多少次，就在timeOfAge数组对应年龄的位置设置多少次
+        for (int j = 0; j < ages.length; j++) {
+            int a = ages[j];
+            timeOfAge[a]++;
+        }
+
+        int index = 0;
+        for (int i = youngAge; i <= oldAge; i++) {// 按照年龄从小到达依次遍历timeOfAge
+            for (int j = 0; j < timeOfAge[i]; j++) {// 在timeOfAge中取得各个年龄位置记录的出现次数
+                ages[index] = i;// 将新数组从头设置出现的年龄，已经排好序
+                index++;
+            }
+        }
+    }
+}
---
+++ src/main/java/leetcode/sort/CountSort.java
@@ -0,0 +1,40 @@
+package leetcode.sort;
+/*************************************************************************
+	> File Name: CountSort.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年11月15日 星期四 18时48分31秒
+ ************************************************************************/
+/**
+ *对于一个输入数组中的一个元素x，只要我们知道了这个数组中比x小的元素的个数，那么我们就可以直接把x放到（x+1）的位置上。这就是计数排序的基本思想。 
+基于这个思想，计数排序的一个主要问题就是如何统计数组中元素的个数。再加上输入数组中的元素都是0-k区间的一个整数这个条件，那么就可以通过另外一个数组的地址表示输入元素的值，数组的值表示元素个数的方法来进行统计。
+ *
+ *
+ */
+
+public class CountSort{
+    public static void countSort(int[] arr,int k){
+        int[] count=new int[k];
+        for(int i:arr){
+            count[i]++;
+        }
+        for(int i=1;i<k;i++){
+            count[i]=count[i]+count[i-1];
+        }
+        int[] arrNew=new int[arr.length];
+        for(int j=arr.length-1;j>=0;j--){
+            arrNew[count[arr[j]]-1]=arrNew[j];
+            count[arr[j]]--;
+        }
+        System.arraycopy(arrNew,0,arr,0,arr.length);
+    }
+    public static void main(String args[]){
+        int k=10;
+        int[] test={1,5,6,7,8,9,0,4,5,6,7,8,2,2,4,5};
+        countSort(test,k);
+        for(int i:test){
+            System.out.println(i);
+        }
+    }
+}
+
